10cb62c05d142514dd4d65e4d1991bc3110ed900
Nicolas Bohocki <65355143+Malrym@users.noreply.github.com>
2020-06-30 13:38:23 +0200

    Update ExcelConverter0.1.1.py

--- ExcelConverter0.1.1.py
+++ ExcelConverter0.1.1.py
@@ -4,18 +4,18 @@ print("Keep in mind, that your Table you want to move your Excel-File to has to 
 print("Right now, the Tool is only Capable of converting one sheet at once.")
 print("to make sure everything is working, please insert first your Username, Password, Host and Database you want to move your Excel-Table to.")
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-import pandas as pd                                                                                                             #
-import mysql.connector as mc                                                                                                    #
-from mysql.connector import errorcode
-import tkinter as tk                                                                                                            #
-from tkinter import filedialog                                                                                                  #
+import pandas as pd                                                                                                             # Importieren verschiedener Module: Pandas zum Einlesen der XLSX-Datei
+import mysql.connector as mc                                                                                                    # MySQLConnetor um eine Verbindung zu MySQL und der Workbench aufzubauen
+from mysql.connector import errorcode                                                                                           # Ausserdem die errorcodes zur Fehleranalyse und Weiterverwendung
+import tkinter as tk                                                                                                            # tkinter um einen Popup-Dialog um den Dateipfad der XLSX-Datei aufzurufen
+from tkinter import filedialog                                                                                                  # 
 from pathlib import Path                                                                                                        #
-import sys
+import sys                                                                                                                      #
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 root = tk.Tk()                                                                                                                  #
 root.attributes("-topmost",True)                                                                                                #
 root.lift()                                                                                                                     #
-root.withdraw()
+root.withdraw()                                                                                                                 #
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 Username = input("Log in as: ")                                                                                                 #
 PW = input("Password: ")                                                                                                        #
@@ -37,29 +37,54 @@ df = pd.read_excel(xlsxProc)                                                    
 PathSchema = input("Schema of your DB: ")                                                                                       #                                                 
 Columnlist = list(df.columns)                                                                                                   #
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-try:
-    cursor.execute("select"+"*"+"from "+PathSchema+"."+Path(xlsxProc).stem+";")
-except mc.Error as err:
-    print("An Error occured: ",err)
-    if err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Theres no existing "+PathSchema+"-Schema")
-        errBadDB = input("Do you want to create a new Standard-Schema as "+PathSchema+"? (yes/no): ")
-        while errBadDB != "yes" and errBadDB != "no":
-            errBadDB = input("Input was not detected, please try again (yes/no): ")
-            print(errBadDB)
-        else:
-            pass
-        if errBadDB == "yes":
-            print("Creating a new "+PathSchema+"-Schema with standard settings")
-            try:
-                cursor.execute("create Schema "+PathSchema)
-            except mc.Error as err:
-                print("An Error occured: "+err+" try restarting the Converter")
-        if errBadDB == str("no"):
-            print("Try Adjusting your inputs and restart the Converter")
-            Stop = input("Press enter to Exit the Converter")
-            if Stop == "" and Stop != "":
-                sys.exit()
+try:                                                                                                                            #
+    cursor.execute("select"+"*"+"from "+PathSchema+"."+Path(xlsxProc).stem+";")                                                 #
+except mc.Error as err:                                                                                                         #
+    print("An Error occured: ",err)                                                                                             #
+    if err.errno == errorcode.ER_BAD_DB_ERROR:                                                                                  # 
+        print("Theres no existing "+PathSchema+"-Schema")                                                                       #
+        errBadDB = input("Do you want to create a new Standard-Schema as "+PathSchema+"? (yes/no): ")                           #
+        while errBadDB != "yes" and errBadDB != "no":                                                                           #
+            errBadDB = input("Input was not detected, please try again (yes/no): ")                                             #
+        else:                                                                                                                   #
+            pass                                                                                                                #
+        if errBadDB == "yes":                                                                                                   #
+            print("Creating a new "+PathSchema+"-Schema with standard settings")                                                #
+            try:                                                                                                                #
+                cursor.execute("create Schema "+PathSchema)                                                                     #
+                connection.commit()                                                                                             #
+            except mc.Error as err:                                                                                             #
+                print("An Error occured: "+err+" try restarting the Converter")                                                 #
+        if errBadDB == str("no"):                                                                                               #
+            print("Try Adjusting your inputs and restart the Converter")                                                        #
+            Stop = input("Press enter to Exit the Converter")                                                                   #
+            if Stop == "" and Stop != "":                                                                                       #
+                sys.exit()                                                                                                      #
+     if err.errno == errorcode.ER_BAD_TABLE_ERROR:                                                                              #
+        print("Theres no existing Table named "+Path(xlsxProc).stem)                                                            #
+        errBadTable = input("Do you want to create a new Table as "+Path(xlsxProc).stem+" within the "+PathSchema+"?")          #
+          while errBadTable != "yes" and errBadTable != "no":                                                                   # 
+            errBadTable = input("Input was not detected, please try again (yes/no): ")                                          #
+        else:                                                                                                                   #
+            pass                                                                                                                #
+        if errBadTable = "yes":                                                                                                 #
+          print("Creating a new Table \""+Path(xlsxProc).stem+"\" within the "+PathSchema+" Schema")                            #
+          try:                                                                                                                  #
+            Columnstring = "("+Columnlist[0]+" varchar(99) primary key unique,"                                                 #
+            for Title in Columnlist:                                                                                            #
+              if Tile = Columnlist[0]:                                                                                          #
+                pass                                                                                                            #
+              else:                                                                                                             #
+                Columnstring += Title+" varchar(99),"                                                                           #
+            Columnstring = Columnstring[0:len(Columnstring)-1]                                                                  #
+            Columnstring += ");"                                                                                                #
+            connection.commit()                                                                                                 #         
+            cursor.execute("create table "+PathSchema+"."+Path(xlsxProc).stem+Columnstring                                      #
+        if errBadTable == "no":                                                                                                 #
+            print("Try Adjusting your inputs and restart the Converter")                                                        #
+            Stop = input("Press enter to Exit the Converter")                                                                   #
+            if Stop == "" and Stop != "":                                                                                       #
+                sys.exit()                                                                                                      #
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 StrColumns = ","                                                                                                                #
 for i in Columnlist:                                                                                                            #
